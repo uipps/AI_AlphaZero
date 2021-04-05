3a07b7a79f9d3341dbc4ad38eefb8ea765737b71
tom-cf <uipps_com@163.com>
2021-04-05 13:22:19 +0800

    带x,y轴，任意y=f(x) 的函数图像；可以设置x,y取值范围和步长！

--- draw_func_graph/f_withX_Y.py
+++ draw_func_graph/f_withX_Y.py
@@ -4,75 +4,81 @@ python3.7   （python2.7报错）
     带箭头x-y坐标轴 , 参考： https://www.jianshu.com/p/af8a448d4d4f
 
 运行：
-  python F:/develope/python/study_python/draw_func_graph/f_withX_Y.py
+  C:/ProgramData/Anaconda3/condabin/conda activate ailearn_py37                                ( python3.7环境下)
+  python F:/develope/python/study_python/draw_func_graph/f_withX_Y.py -f "1/x" -y 5 -x 1
+  python F:/develope/python/study_python/draw_func_graph/f_withX_Y.py -f "2*x" -y 5 -x 2
+
+  -- 设定x,y的取值范围和步长s
+  python F:/develope/python/study_python/draw_func_graph/f_withX_Y.py -f "1/3*x*x*x - x" -x 2 -y 1 -s 0.001
 
 '''
 
 import matplotlib.pyplot as plt  #导入matplotlib库
 import numpy as np  #导入numpy库
 import mpl_toolkits.axisartist as axisartist
-
-'''
-# 画 y = cos(x) 图像
-x=np.linspace(-3*np.pi,3*np.pi,256,endpoint=80) #图像横坐标范围
-y=np.cos(x)  # y = cos(x)
-plt.plot(x,y,color='red',linewidth=5,linestyle='-') #根据x y 数组绘制直线曲线
-plt.show()
-'''
-
-'''
-#没有x，y坐标轴的情况
-
-#生成x步长为0.1的列表数据
-x = np.arange(-15,15,0.1)
-#plt.xlim(-12,12)		#设置x、y坐标轴的范围
-#plt.ylim(-1, 1)
-
-#生成 sigmiod 形式的y数据
-y=1/(1+np.exp(-x))
-
-#绘制图形
-plt.plot(x,y, c='b')
-plt.show()
-'''
+import argparse
 
 
-## 1.创建画布并引入axisartist工具。
-#创建画布
-fig = plt.figure(figsize=(8, 8))
-#使用axisartist.Subplot方法创建一个绘图区对象ax
-ax = axisartist.Subplot(fig, 111)  
-#将绘图区对象添加到画布中
-fig.add_axes(ax)
+def run():
+    ''' 获取命令行参数 '''
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', type=str, default='2*x', help='draw y=2x')
+    #parser.add_argument('-f', dest='y=f(x), function ', action='store',nargs='?', default='2x', type=str, required=False, help='draw y=2x')
+    parser.add_argument('-b', type=float, help='xrange, begin, default -2')
+    parser.add_argument('-x', type=float, default=2, help='xrange, end, default 2')
+    parser.add_argument('-y', type=float, default=2, help='y-Range, begin and end, default 2')
+    parser.add_argument('-s', type=float, default=0.01, help='x-step, end, default 0.1')
+
+    args = parser.parse_args()
+    yfunc = args.f
+    xBegin = args.b
+    xEnd = args.x         # x-range , max
+    yMax = args.y
+    xStep = args.s
+    #print(yfunc)
+    if not xBegin:
+        xBegin = -1 * xEnd	# 默认对称，取x的反
+
+    ''' 画函数图 '''
+    ## 1.创建画布并引入axisartist工具。
+    #创建画布
+    fig = plt.figure(figsize=(8, 8))
+    #使用axisartist.Subplot方法创建一个绘图区对象ax
+    ax = axisartist.Subplot(fig, 111)  
+    #将绘图区对象添加到画布中
+    fig.add_axes(ax)
+
+    ## 2.绘制带箭头的x-y坐标轴
+    #通过set_visible方法设置绘图区所有坐标轴隐藏
+    ax.axis[:].set_visible(False)
+
+    #ax.new_floating_axis代表添加新的坐标轴
+    ax.axis["x"] = ax.new_floating_axis(0,0)
+    #给x坐标轴加上箭头
+    ax.axis["x"].set_axisline_style("->", size = 1.0)
+    #添加y坐标轴，且加上箭头
+    ax.axis["y"] = ax.new_floating_axis(1,0)
+    ax.axis["y"].set_axisline_style("-|>", size = 1.0)
+    #设置x、y轴上刻度显示方向
+    ax.axis["x"].set_axis_direction("top")
+    ax.axis["y"].set_axis_direction("right")
 
 
+    ## 3.在带箭头的x-y坐标轴背景下，绘制函数图像
+    #生成x步长为0.1的列表数据
+    x = np.arange(xBegin, xEnd, xStep)
+    #plt.xlim(xBegin, xEnd)        #设置x、y坐标轴的范围
+    plt.ylim(-1*yMax, yMax)
 
-## 2.绘制带箭头的x-y坐标轴
-#通过set_visible方法设置绘图区所有坐标轴隐藏
-ax.axis[:].set_visible(False)
+    #生成sigmiod形式的y数据
+    #y=1/(1+np.exp(-x))
+    #y=1/3*x*x*x - x  # y = 1/3*x^3 - x
+    y=eval(yfunc)		# 字符串转表达式
+    #y=1/x
 
-#ax.new_floating_axis代表添加新的坐标轴
-ax.axis["x"] = ax.new_floating_axis(0,0)
-#给x坐标轴加上箭头
-ax.axis["x"].set_axisline_style("->", size = 1.0)
-#添加y坐标轴，且加上箭头
-ax.axis["y"] = ax.new_floating_axis(1,0)
-ax.axis["y"].set_axisline_style("-|>", size = 1.0)
-#设置x、y轴上刻度显示方向
-ax.axis["x"].set_axis_direction("top")
-ax.axis["y"].set_axis_direction("right")
+    #绘制图形
+    plt.plot(x,y, c='b')
+    plt.show()
 
-
-## 3.在带箭头的x-y坐标轴背景下，绘制函数图像
-#生成x步长为0.1的列表数据
-x = np.arange(-2,2,0.01)
-#plt.xlim(-12,12)		#设置x、y坐标轴的范围
-#plt.ylim(-1, 1)	
-
-#生成sigmiod形式的y数据
-#y=1/(1+np.exp(-x))
-y=1/3*x*x*x - x  # y = 1/3*x^3 - x
-
-#绘制图形
-plt.plot(x,y, c='b')
-plt.show()
+if __name__ == '__main__':
+    run()
