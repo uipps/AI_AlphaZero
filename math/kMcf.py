ade08d4e6dd194be65df972d0d5d616fdeb9f87d
cf <uipps_com@163.com>
2024-07-05 21:09:56 +0800

    -s参数改成-l表示结果保留多少长度的小数位数

--- math/kMcf.py
+++ math/kMcf.py
@@ -8,11 +8,11 @@ python3.7
   conda install -y sympy
 
   参数说明：
-    -d √2, 开方底数，默认2
+    -d  √n, 就是开方底数n，默认2
     -m  任意实数n的m次方根，这里m必须是大于1的正整数，
-    -s  计算精度，类似于php中bcscale小数点位数，默认100
+    -l    计算精度，结果保留多少小数点位数，默认100
 
-  python F:/develope/python/study_python/math/kMcf.py -s 120 -d 2 -m 30 
+  python F:/develope/python/study_python/math/kMcf.py -l 120 -d 2 -m 30 
 
 '''
 
@@ -29,22 +29,22 @@ def run():
     ''' 获取命令行参数 '''
     parser = argparse.ArgumentParser()
     #parser.add_argument('-d', type=str, default='x**2/2', help='draw y=x**2/2')
-    parser.add_argument('-d', type=float, default=2, help='m√d, the d, default 2')
-    parser.add_argument('-m', type=float, default=20, help='m√d,  the m , default 20')
-    parser.add_argument('-s', type=float, default=100, help='scale, bcscale, the num length behind dot., default 120')
+    parser.add_argument('-d', type=int, default=2, help='m√d, the d, default 2')
+    parser.add_argument('-m', type=int, default=20, help='m√d,  the m , default 20')
+    parser.add_argument('-l', type=int, default=100, help='scale, the num length behind dot., default 100')
 
     args = parser.parse_args()
     dishu = args.d
     kaifangci = args.m
-    bcscale = args.s
+    rlt_len = args.l
 
-    if bcscale < 0 or dishu < 0 or kaifangci < 0 :
+    if rlt_len < 0 or dishu < 0 or kaifangci < 0 :
         print(" error! ") 
         return 0
 
-    mp.dps = bcscale  # 设置精度为 1000 位小数
+    mp.dps = rlt_len  # 设置保留小数点后 100 位小数
     result_root = high_precision_nth_root(dishu, kaifangci)
-    #print("2 的 30 次方根（精确到 1000 位）：", result_30)
+    #print("2 的 30 次方根（精确到 100 位）：", result_root)
     print(result_root)
 
 if __name__ == '__main__':
